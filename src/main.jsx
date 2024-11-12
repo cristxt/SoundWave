42b22ae64aa3ddbea3017233b0e11176ed632a10
Cristina Romero <criromllo@gmail.com>
2024-11-12 20:47:15 +0100

    imagenes, circulos y fondo

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -7,4 +8,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
