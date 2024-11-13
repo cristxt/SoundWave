ac2f6b52d7cd853c3f58bba43097e7993bc2310b
Cristina Romero <criromllo@gmail.com>
2024-11-13 17:20:28 +0100

    rutas

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,11 @@
-import FeelTheMusicLayout from "../../SoundWave/src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout";
+/*import FeelTheMusicLayout from "../../SoundWave/src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout";*/
 import { createBrowserRouter } from 'react-router-dom';
 import DiscoverNewMusicLayout from "./assets/components/pages/DiscoverNewMusic/DiscoverNewMusicLayout";
 export const router = createBrowserRouter([
-    {
+    /*{
         path: "/feel-the-music",
         element: <FeelTheMusicLayout />
-    },
+    },*/
     {
         path: "/discover-new-music",
         element: <DiscoverNewMusicLayout/>
