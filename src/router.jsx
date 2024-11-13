a7ff41a9c71385d8e4fc2b9a8d878c85179ca3dd
Cristina Romero <criromllo@gmail.com>
2024-11-13 16:58:11 +0100

    unificacion rutas

--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,18 @@
 import FeelTheMusicLayout from "../../SoundWave/src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout";
 import { createBrowserRouter } from 'react-router-dom';
+import DiscoverNewMusicLayout from "./assets/components/pages/DiscoverNewMusic/DiscoverNewMusicLayout";
 export const router = createBrowserRouter([
     {
         path: "/feel-the-music",
         element: <FeelTheMusicLayout />
     },
-    /*
-    paths para incluir en mis siguients commits
     {
-        path: "/"",
-        element: </>
+        path: "/discover-new-music",
+        element: <DiscoverNewMusicLayout/>
     },
     {
-        path: "/",  
+        path: "/unknow",  
         element: </>
-    }*/
+    }
 ]);
 
