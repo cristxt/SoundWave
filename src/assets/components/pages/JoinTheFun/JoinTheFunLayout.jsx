004cd52bb11e5ceba78eaeaf5c69b4d330941565
Cristina Romero <criromllo@gmail.com>
2024-11-14 12:15:25 +0100

    landing 3 casi

--- src/assets/components/pages/JoinTheFun/JoinTheFunLayout.jsx
+++ src/assets/components/pages/JoinTheFun/JoinTheFunLayout.jsx
@@ -10,12 +10,12 @@ const JoinTheFunLayout = () => {
             <div className="container3">
             <div className="circle3"></div>
             <div className="circle4"></div>
-                <div className="left-container3">
-                    <h1 class="h13">Join The Fun</h1>
-                </div>
-                <div className="right-container3">
-                <Form/> 
-                </div>
+            <div className="left-container3">
+                <h1 class="h13">Join The <span class="color">fun.</span></h1>
+            </div>
+            <div className="right-container3">
+            <Form/> 
+            </div>
             </div>
         </div>
     );
