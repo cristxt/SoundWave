363c64b2a998817784fc41152a9927d8d9aaec44
Cristina Romero <criromllo@gmail.com>
2024-11-14 09:58:40 +0100

    ajustes en router y main landing 1

--- src/router.jsx
+++ src/router.jsx
@@ -1,18 +1,20 @@
-import FeelTheMusicLayout from "../../SoundWave/src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout";
 import { createBrowserRouter } from 'react-router-dom';
-/*import DiscoverNewMusicLayout from "./assets/components/pages/DiscoverNewMusic/DiscoverNewMusicLayout";*/
+import FeelTheMusicLayout from "../../SoundWave/src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout";
+import DiscoverNewMusicLayout from "../src/assets/components/pages/DiscoverNewMusic/DiscoverNewMusicLayout";
+import JoinTheFunLayoutLayout from "./assets/components/pages/JoinTheFun/JoinTheFunLayout";
+
 export const router = createBrowserRouter([
     {
         path: "/feel-the-music",
         element: <FeelTheMusicLayout />
     },
-    /*{
+    {
         path: "/discover-new-music",
         element: <DiscoverNewMusicLayout/>
     },
     {
-        path: "/unknow",  
-        element: </>
-    }*/
+        path: "/join-the-fun",  
+        element: <JoinTheFunLayoutLayout/>
+    }
 ]);
 
