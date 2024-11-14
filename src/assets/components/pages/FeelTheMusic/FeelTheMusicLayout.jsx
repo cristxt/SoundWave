2364862504e7a6d1d3c844355d0e4ee48a41ed16
Cristina Romero <criromllo@gmail.com>
2024-11-14 10:02:06 +0100

    ajustes en router y main landing 2

--- src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout.jsx
+++ src/assets/components/pages/FeelTheMusic/FeelTheMusicLayout.jsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import './FeelTheMusicLayout.css';
-import JoinButton from '../../Atoms/button/joinButton';
-import Nav from '../../Molecules/nav';
 
-const FeelTheMusicLayout = () => {
-    return (
-        <div className="feel-the-music">
-            <Nav />
-            <h1>Feel the Music</h1>
-            <JoinButton />
-        </div>
-    );
-}
-
-export default FeelTheMusicLayout;
